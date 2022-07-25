6e5cfdd49f966aeacd43e43d610fecddb24b2d54
Arian Weslley <weslleysantoszbd@gmail.com>
2022-07-25 14:24:07 -0300

    Finalização

---
+++ Loop_Simples/src/App.java
@@ -0,0 +1,15 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		String nome = "Navelogic";
+		int i = 0;
+		
+		while(i < 5) {
+			System.out.println("Meu nome é: " + nome);
+			i++;
+		};
+
+	}
+
+}
